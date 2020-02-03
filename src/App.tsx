fbd2785bc4e89b1905876974e2de1eb89daf340e
Hugh Rawlinson <hughr2005@gmail.com>
2020-02-02 23:52:05 -0500

    More typescriptizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React, {useRef, useEffect, useState} from 'react';
+import React, {useRef, useEffect, useState, ChangeEvent } from 'react';
+import { MeydaAudioFeature, MeydaFeaturesObject } from 'meyda';
+import CSS from 'csstype';
 import { extractFeature } from 'feature-extractor-worker';
 
-function normalize(array) {
+function normalize(array: number[]) {
   const maxVal = Math.max.apply(null,array);
   return array.map(val => val / maxVal);
+}
+
+type LineProps = {
+  signal: number[],
+  label: string,
+  style: CSS.Properties
 }
 
 /**
@@ -11,17 +19,22 @@ function normalize(array) {
  * @param options.signal An ArrayLike of numbers
  * @param options.label A string to be shown as a label of the signal
  */
-function Line({signal, label}) {
+function Line({signal, label}: LineProps) {
   const height = 150;
   let [zoom, setZoom] = useState(0);
 
-  const scrollRef = useRef(null)
+  const scrollRef = useRef(null as (SVGSVGElement | null));
   useEffect(() => {
-    const scrollEl = scrollRef.current
-    scrollEl.addEventListener('wheel', stopScroll)
-    return () => scrollEl.removeEventListener('wheel', stopScroll)
+    const scrollEl = scrollRef?.current;
+    scrollEl?.addEventListener('wheel', stopScroll);
+    return () => scrollEl?.removeEventListener('wheel', stopScroll);
   }, [])
-  const stopScroll = e => e.preventDefault()
+  const stopScroll = (e: Event) => e.preventDefault()
+
+  function handleScroll(event: React.WheelEvent) {
+    event.preventDefault();
+    setZoom(zoom - event.deltaY);
+  }
 
   const points = normalize(signal)
     .reduce(({points, previous}, value, index) => ({
@@ -31,12 +44,6 @@ function Line({signal, label}) {
       "points": `0,${height} `,
       "previous": 0
     }).points + `${signal.length},${height}`;
-
-  function handleScroll(event) {
-    event.preventDefault();
-    setZoom(zoom - event.deltaY);
-    return false;
-  }
 
   return (
     <svg
@@ -63,34 +70,56 @@ function Line({signal, label}) {
 }
 
 function App() {
-  const [signals, updateSignal] = useState(null);
+  type SignalsType = Map<MeydaAudioFeature, number[]>;
+  const [signals, updateSignal] = useState(null as SignalsType | null);
 
-  const features = ['loudness'];
+  const features = ['loudness' as MeydaAudioFeature];
 
-  async function inputChangeHandler(event) {
-    console.log(event.target.files[0]);
-    const loudness = await extractFeature({
-      audioBlob: event.target.files[0],
-      audioFeatures: features,
-      extractionParams: {
-        channels: [0],
-        bufferSize: 2048
+  async function inputChangeHandler(event: ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (file) {
+      const loudness: (Partial<MeydaFeaturesObject> | null)[][] = await extractFeature({
+        audioBlob: file,
+        audioFeatures: features,
+        extractionParams: {
+          channels: [0],
+          bufferSize: 2048,
+          hopSize: 0,
+        }
+      });
+
+      const featuresForSignal = loudness[0];
+
+      if (featuresForSignal[0]) {
+        const mapper = (key: string): [string, number[]] => {
+          return [key, []];
+        }
+
+        const returnedFeatures = Object.keys(featuresForSignal[0])
+
+        let newSignals: {[x: string]: number[]} = Object.fromEntries(
+          returnedFeatures.map(mapper)
+        );
+
+        let isLoudnessResult = (p: any): p is {specific: Float32Array, total: number} => !! p.total;
+
+        for (let i = 0; i < featuresForSignal.length; i++) {
+          const featureForFrame = featuresForSignal[i];
+          if (featureForFrame) {
+            Object.entries(featureForFrame).forEach(([name, value]) => {
+              if (typeof value === "number") {
+                newSignals[name].push(value);
+              } else if (isLoudnessResult(value)) {
+                newSignals[name].push(value.total);
+              } else {
+                newSignals[name].push(0);
+              }
+            })
+          }
+        }
+        updateSignal(() => newSignals as unknown as SignalsType);
       }
-    });
-
-    const newSignals = loudness[0].reduce((acc, el) =>
-    features.reduce((featuresAccumulator, featureName) => ({
-      ...featuresAccumulator,
-      [featureName]: [...acc[featureName], el[featureName]]
-    }), {}),
-      features.reduce((featuresAccumulator, featureName) => ({
-        ...featuresAccumulator,
-        [featureName]: []
-      }), {})
-    );
-
-    console.log(newSignals);
-    updateSignal(newSignals);
+    }
   }
 
   return (
